5aa211f9aa8fce90b27e735fd667fb48f2ff67ae
Katie Jones <katie.jones@reactive-robotics.com>
2018-05-24 10:23:39 +0200

    Change copy/paste shortcuts

--- config.def.h
+++ config.def.h
@@ -169,8 +169,8 @@ static Shortcut shortcuts[] = {
 	{ MODKEY|ShiftMask,     XK_Home,        xzoomreset,     {.f =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ MODKEY|ShiftMask,     XK_Insert,      clippaste,      {.i =  0} },
-	{ MODKEY|ShiftMask,     XK_C,           clipcopy,       {.i =  0} },
-	{ MODKEY|ShiftMask,     XK_V,           clippaste,      {.i =  0} },
+	{ ControlMask|ShiftMask,     XK_Z,           clipcopy,       {.i =  0} },
+	{ ControlMask|ShiftMask,     XK_X,           clippaste,      {.i =  0} },
 	{ MODKEY,               XK_Num_Lock,    numlock,        {.i =  0} },
 };
 
